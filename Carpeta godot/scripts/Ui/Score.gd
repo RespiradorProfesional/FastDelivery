c730a38b57c980ab216530518690e27823200916
Néstor Álvarez <116906280+Nstorlvarez@users.noreply.github.com>
2024-05-07 16:26:30 +0100

    timer y tal hecho

--- Carpeta godot/scripts/Ui/Score.gd
+++ Carpeta godot/scripts/Ui/Score.gd
@@ -1,11 +1,30 @@
 extends Node2D
 
-
+@onready var timer=$Timer
+var seconds
+@onready var cronometre=$CanvasLayer/Label
+@onready var finalLevel=$finalLevel
+var tiempoFormateado
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tiempoFormateado= "0:00"
+	seconds=0
+
+func _on_timer_timeout():
+	seconds+=1
+	# Suponiendo que "segundosTotales" es el tiempo total en segundos
+	var minutos = seconds / 60
+	var segundos = seconds % 60
+	# Formatear los minutos y segundos en una cadena
+	if segundos < 10:
+		tiempoFormateado = str(minutos) + ": 0" + str(segundos)
+		cronometre.text=tiempoFormateado
+	else : 
+		tiempoFormateado = str(minutos) + ":" + str(segundos)
+		cronometre.text=tiempoFormateado
+	cronometre.text=tiempoFormateado
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_final_level_body_entered(body):
+	seconds=0
